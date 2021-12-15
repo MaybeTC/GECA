66739fe534be03f10f756efd4436ce6534add3de
MaybeTC <d16122910@mydit.ie>
2021-12-15 02:49:44 +0000

    W/S Keyboard
    Make car acceleration and deceleration function.
    W key is acceleration and S key is deceleration.

--- CyberpunkFormulaRacing/Assets/Scripts/Car.cs
+++ CyberpunkFormulaRacing/Assets/Scripts/Car.cs
@@ -6,13 +6,13 @@ using UnityEngine.SceneManagement;
 public class Car : MonoBehaviour
 {
     //Setting speed
-    public float speed = 10;
+    public float speed;
     public float turnSpeed = 5;
     public float fallingSpeed = -12;
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = 1;
     }
 
     // Update is called once per frame
@@ -29,7 +29,20 @@ public class Car : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         //Using Time.deltaTime to control the speed can be independent of the frame rate of the device.
         transform.Translate(x * turnSpeed * Time.deltaTime, 0, speed * Time.deltaTime);
+        
+        //Press W key to accelerate
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            speed = 20;
+            return;
+        }
 
+        //Press S key to slow down.
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            speed = 5;
+            return;
+        }
 
         //When the car moves off the track
         if(transform.position.x < -4.5 || transform.position.x > 4.5)
