fba4303e968948d893a6c18531527f09f56884f5
MaybeTC <d16122910@mydit.ie>
2021-12-15 00:31:24 +0000

    Car Setting
    Add the mass and collision volume of the car, keep the car moving forward, and add the function of moving left and right. Add roadblocks.
    I updated the project name again.

---
+++ CyberpunkFormulaRacing/Assets/Scripts/Car.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    //Setting speed
+    public float speed = 15;
+    public float turnSpeed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get left and right input of keyboard
+        float x = Input.GetAxis("Horizontal");
+        //Using Time.deltaTime to control the speed can be independent of the frame rate of the device.
+        transform.Translate(x * turnSpeed * Time.deltaTime,0,speed * Time.deltaTime);
+    }
+}
