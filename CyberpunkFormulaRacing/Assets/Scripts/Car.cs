a6f6bba7254800ccd8520a47da0f16c565c3a49f
MaybeTC <d16122910@mydit.ie>
2021-12-15 02:37:50 +0000

    enemy navigate
    Set up automatic pathfinding for the enemy. If the enemy catches up with you, you will lose the game.

--- CyberpunkFormulaRacing/Assets/Scripts/Car.cs
+++ CyberpunkFormulaRacing/Assets/Scripts/Car.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class Car : MonoBehaviour
 {
     //Setting speed
-    public float speed = 15;
+    public float speed = 10;
     public float turnSpeed = 5;
     public float fallingSpeed = -12;
     // Start is called before the first frame update
@@ -30,12 +30,15 @@ public class Car : MonoBehaviour
         //Using Time.deltaTime to control the speed can be independent of the frame rate of the device.
         transform.Translate(x * turnSpeed * Time.deltaTime, 0, speed * Time.deltaTime);
 
-        //
+
+        //When the car moves off the track
         if(transform.position.x < -4.5 || transform.position.x > 4.5)
         {
+            //The car began to fall.
             transform.Translate(0, fallingSpeed * Time.deltaTime, 0);
         }
-        //
+        
+        //After that lose the game.
         if(transform.position.y < -25)
         {
             Debug.Log("You Lose! Press R key to restart the game.");
---
+++ CyberpunkFormulaRacing/Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform goal;
+    public NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //
+        if(other.name == "CarBodywork")
+        {
+            Debug.Log("You lose!");
+            Time.timeScale = 0;
+        }
+        
+    }
+}
